bec0a4156c370057ab78e08fe7dcaa41137413c9
donwany <theodondre@gmail.com>
2022-08-15 18:29:21 -0500

    first commit

--- main.py
+++ main.py
@@ -3,33 +3,36 @@ from datetime import date, timedelta
 
 import requests
 
-if len(sys.argv) != 2:
-    raise SystemExit("Usage python main.py <COUNTRY>")
-else:
-    # To fetch the latest total confirmed cases number you need to pass a 24h
-    # window to the API. That's why you need to pass both yesterday's date
-    # and today's.
-    today = date.today()
-    yesterday = today - timedelta(days=1)
+def main():
+    if len(sys.argv) != 2:
+        raise SystemExit("Usage python main.py <COUNTRY>")
+    else:
+        # To fetch the latest total confirmed cases number you need to pass a 24h
+        # window to the API. That's why you need to pass both yesterday's date
+        # and today's.
+        today = date.today()
+        yesterday = today - timedelta(days=1)
 
-    # Pick a country slug from the list: https://api.covid19api.com/countries
-    country = sys.argv[1]
+        # Pick a country slug from the list: https://api.covid19api.com/countries
+        country = sys.argv[1]
 
-    endpoint = f"https://api.covid19api.com/country/{country}/status/confirmed"
-    params = {"from": str(yesterday), "to": str(today)}
+        endpoint = f"https://api.covid19api.com/country/{country}/status/confirmed"
+        params = {"from": str(yesterday), "to": str(today)}
 
-    # The response will be a list of results per day,
-    # in your case only 1 result.
-    response = requests.get(endpoint, params=params).json()
+        # The response will be a list of results per day,
+        # in your case only 1 result.
+        response = requests.get(endpoint, params=params).json()
 
-    # Finally, you need to traverse through the response and increment the
-    # `total_confirmed` variable with the total number of confirmed cases
-    # available that day, which is in the field `Cases`.
-    total_confirmed = 0
+        # Finally, you need to traverse through the response and increment the
+        # `total_confirmed` variable with the total number of confirmed cases
+        # available that day, which is in the field `Cases`.
+        total_confirmed = 0
 
-    for day in response:
-        cases = day.get("Cases", 0)
-        total_confirmed += cases
+        for day in response:
+            cases = day.get("Cases", 0)
+            total_confirmed += cases
+
+    print(f"Total Confirmed Covid-19 cases in {country}: {total_confirmed}")
 
 if __name__ == "__main__":
-    print(f"Total Confirmed Covid-19 cases in {country}: {total_confirmed}")
+    main()
